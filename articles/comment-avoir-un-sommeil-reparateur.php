b944f2bd3640eb267500c76e691a0fc8ec13a6d4
unknown <romain.geffrault@gmail.com>
2020-03-26 17:07:15 +0100

    add first article

---
+++ articles/comment-avoir-un-sommeil-reparateur.php
@@ -0,0 +1,186 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, OPTIONS');
+header('Access-Control-Allow-Headers: responsetype');
+?>
+
+<h1>Comment avoir un sommeil réparateur ?</h1>
+
+<img class="imgBook noMarginBottom" src="assets/img/sommeil-reparateur.jpg" alt="Tête réparé" >
+<div class="creditImgDiv"><a class="creditImg" href="https://fr.freepik.com/photos-vecteurs-libre/fond" target="_blank" rel="nofollow">Lien Créateur Image</a></div>
+
+<p>Avoir un sommeil réparateur est très important que se soit pour se sentir bien, se sentir en forme, être prêts à affronter la journée, avoir récupéré des efforts de la veille, pour apprendre et faire le trie des informations de la journée.</p>
+<p>À l'inverse mal dormir, ne permet pas de bénéficier de tous les effets d'un bon sommeil.</p>
+<p>Ca tombe bien, nous allons voir dans cet article comment avoir un sommeil réparateur !</p>
+
+<h2>Sommeil réparateur où es-tu?</h2>
+<p>Ayant eu pendant longtemps des problèmes de sommeil.</p>
+<p>Je me suis intéressé à comment je pouvait faire pour avoir un sommeil réparateur.</p>
+<p>Je voulais simplement me réveiller le matin avec toute mon énergie et avoir bien récupéré des efforts de la veille.</p>
+<p>Je me suis d'abord questionné sur l'effet réparateur du sommeil.</p>
+
+<h2>Comment le corps récupère ?</h2>
+
+<p>Lorsque l'on dort, le corps lance un processus de nettoyage qui élimine les toxines et autres déchets.</p>
+<p>C'est de là que viendrait la sensation de bien récupérer.</p>
+<p>Le corps se distingue en deux parties pour la gestion de l'élimination des déchets.
+<p>Le corps et le cerveau.</p>
+
+
+<h3>Déchets générés par le cerveau</h3>
+<p>Une étude récente (<a rel="nofollow" target="_blank" href="https://science.sciencemag.org/content/342/6156/373">ici</a>) à montrer que le cerveau possède son propre système de nettoyage.</p>
+<p>Il s'agit du système glymphatique.</p>
+<p>Ca fait très médicale comme nom et ce n'est pas très parlant.</p>
+<p>On va prendre l'exemple des éboueurs.</p>
+<p>Le système glymphatique va représenter un camion poubelle.</p>
+<p>Il va être alimenté par les éboueurs qui circulent dans l'espace entre les différentes cellules du cerveau à la recherche des déchets.</p>
+
+<p>Cette équipe d'éboueurs fonctionne 24h/24. Mais est 10 fois plus performante lorsque l'on dort.</p>
+<p>L'étude montre que lorsque l'on dort, l'espace entre les cellules augmente de 60%.</p>
+<p>Ca permet aux éboueurs d'avoir plus de place pour chercher et transporter les poubelles. Ce qui leur rend la tâche bien plus facile.</p>
+<p>Dormir va donc permettre de rendre la tâche de nettoyage plus efficace.</p>
+
+<h3>Déchets générés par le corps</h3>
+<p>Le corps possède son propre camion poubelle, le système lymphatique.</p>
+<p>De la même façon, il va récupérer les poubelles des cellules du corps.</p>
+
+<p>Où vont les déchets récupérés par les camions poubelles ?</p>
+
+<p>Une fois remplis les camions poubelles vont prendre l'autoroute en passant par le sang.</p>
+<p>Leur destination est le foie qui fait office de déchetterie.</p>
+
+<h3>Le foie alias Anti-poison Man</h3>
+
+<p>Le foie se charge de traiter l'ensemble des toxines du corps.
+<p>Il va les rendre moins nocives et les rejeter à grâce aux fameux pipi/caca.</p>
+<p>Le foie est un super héros (Anti-poison Man). En plus d'éliminer les toxines des cellules.</p>
+<p>Il va aussi traiter les produits nocifs qui sont passés dans notre corps, comme par l'alimentation.</p>
+<p>Exhausteur de goûts, colorants, antibiotiques (se trouvant dans la viande) et les métaux lourds.</p>
+<p>Les produits chimiques d'entretien, cosmétique, alcool, drogue, médicaments, la poulltion de l'air passée par nos poumons...</p>
+
+<p>Il faut donc veiller au bon fonctionnement de notre foie en apportant une alimentation saine et équilibré pour qu'il puisse se concentrer davantage sur l'élimination des déchets du corps.</p>
+
+<h3>Régénérer nos tissus</h3>
+<p>Tout au long de la journée nos tissus cellulaire comme les muscles se sont abîmés.</p>
+<p>Le sommeil joue un rôle esentiel pour la réparation de nos tissus.</p>
+<p>Comme le nettoyage des déchets, la réparation est d'autant plus efficace la nuit. D'une part car nous ne faisons plus d'activité physique qui abîme les tissus.</p>
+<p>Mais aussi parce que le corps va secreter un cocktail d'hormone spécialement pour se réparer et se consolider.</p>
+<p>Comme l'hormone de croissance, une des plus importante et puissante du corps.</p>
+
+<p>Voyons un peu ce que fait l'hormone de croissance.</p>
+
+<p>Lorsque l'on est bébé, on secrète énormément d'hormone de croissance afin de nous aider à grandir.</p>
+<p>Puis à l'adolescence le corps produit un 2ième pic, encore une fois pour grandir.</p>
+<p>Ensuite, le corps continu à produire de l'hormone de croissance, mais à plus faible dose, pour se maintenir.</p>
+
+<p>Beaucoup de sportifs surtout dans les sports de force comme le rugby ou la musculation peuvent être tentés de prendre de l'hormone de croissance pour ces multiples bénéfices.</p>
+
+<p>Les effets positifs de cette hormone sont :</p>
+<ul>
+	<li>Augmentation de la masse musculaire</li>
+	<li>Diminution de la masse grasse</li>
+	<li>Récupération plus rapide</li>
+	<li>Effets « anti vieillissement » : amélioration de la libido, de l'aspect de la peau...</li>
+	<li>Permet de se sentir bien mentalement</li>
+</ul>
+
+<p>C'est exactement ces effets que l'on recherche avec une bonne nuit de sommeil.</p>
+
+<p>Par contre, les effets négatifs peuvent être très graves, voir mortels.</p>
+<p>Tant que vous laisser le corps faire, vous ne risquez rien.</p>
+<p>Le corps sait s'auto réguler pour fonctionner au mieux.</p>
+<p>C'est lorsqu'une hormone vient d'autre part que par le corps que ça devient très dangereux. C'est un peu faire un pacte avec le diable.</p>
+<p>C'est ce qu'on appelle le dopage.</p>
+<p>Je suis totalement contre ce genre de pratique qui n'est pas bénéfique pour le bien-être et la bonne santé sur le long terme.</p>
+<p>De plus, les raisons d'un tel acte sont très souvent mesquines et hypocrites.</p>
+<p>Actuellement, on peut voir beaucoup d'influenceurs (homme et femme) du fitness qui se dopent, mais qui le cachent.</p>
+<p>Ils mettent en avant leurs résultats grâce à leur méthode d'entraînements et leurs assiduités...</p>
+<p>Bref attention à ce genre de personne. Vous n'aurez jamais leurs résultats et vous risquez de vous abîmer.</p>
+
+	<p class="citation">10% de sportifs amateurs sont dopés</p>
+
+<p>Dans notre cas, pour favoriser la récupération. On va chercher à favoriser la sécrétion naturelle de cette hormone.</p>
+<p>Voici les différents facteurs à optimiser pour favoriser la production d'hormones de croissance :</p>
+<p>Encore une fois, une bonne alimentation est nécessaire (évitez surtout la junk food).</p>
+<p>Perdre du poids, un excès de masse grasse abdominale inhibe la production d'hormone de croissance.</p>
+<p>Faire du sport à forte intensité comme de la musculation.</p>
+<p>Et le dernier point, avoir un sommeil de qualité.</p>
+
+<p>On revient à l'importance du sommeil pour récupérer.</p>
+<p><strong>70% de la sécrétion de cette hormone se fait pendant le sommeil</strong>.</p>
+<p>En particulier lors des phases de <strong>sommeil profond</strong>.</p>
+
+<h2>Les principales phases du sommeil</h2>
+
+<p>Le sommeil se découpe en 4 à 5 cycles de 90 min chacun.</p>
+<p>Chaque cycle se décompose en 5 partie.</p>
+
+<h3>Phases 1 et 2 : Phases d'endormissement !</h3>
+<p>Ces phases représentent 50% du sommeil.</p>
+<p>Le corps relâche la tension et le stress.</p>
+<p>C'est la phase d'échauffement au sommeil.</p>
+
+<h3>Phases 3-4: Sommeil Profond</h3>
+<p>Dans cette phase, le corps ne bouge plus, sa température est la plus basse.</p>
+<p>C'est à cette phase que le corps produits le plus d'hormones dont l'hormone de croissance.</p>
+<p>Ces phases représentent 20% du sommeil.</p>
+<p>À noter que c'est lors du premier cycle où elle dure le plus longtemps.</p>
+<p>Puis, plus les cycles avancent plus les phases de sommeil profond sont courtes et laissent place à plus de sommeil paradoxal.</p>
+
+<p>C'est donc cette phase-là qu'il faut absolument faire pour que le corps puisse récupérer au mieux.</p>
+<p>Car oui, on peut la louper.</p>
+<p>On va voir ça plus loin.</p>
+
+<h3>Phase 5: Sommeil paradoxal</h3>
+<p>C'est la phase où l'on rêve. On passe en moyenne 2h par nuit à rêver, mais si on ne s'en souvient rarement.</p>
+<p>A l'inverse du sommeil profond, plus les cycles du sommeil avance plus le temps passé en sommeil paradoxal augmente.</p>
+<p>Jusqu'à 45min de sommeil paradoxal, soit la moitié d'un cycle.</p>
+<p>C'est aussi une phase très importante pour récupérer, car c'est grâce à cette phase que l'on apprend.</p>
+<p>Le sommeil paradoxal sollicite énormément les parties du cerveau qui génèrent nos pensées et qui organisent les données.</p>
+<p>Les bébés passent plus de temps en phase de sommeil paradoxal, c'est ce qui leur permet d'apprendre énormément.</p>
+
+<p>Cette étude (<a rel="nofollow" target="_blank" href="https://www.ncbi.nlm.nih.gov/pubmed/25447376">ici</a>), montre que les personnes qui sont privé de cette phase de sommeil n'apprenne pas ce qu'ils ont appris à faire la veille.</p>
+<p>Hors ceux privé du sommeil profond ont appris ce qu'ils ont vue la veille.</p>
+
+<p>En période d'examen, penser à ça et si les bénéfices d'une nuit blanche avec l'examen est une bonne idée.</p>
+
+<p>On peut se dire ok, ça permet d'apprendre, mais à mon âge, je n'ai plus besoin d'apprendre. Je peux me passer du sommeil paradoxal.</p>
+<p>Eh bien, je vous dirai, c'est une excellente idée pour se saboter la santé.</p>
+
+<p>Beaucoup d'étude montrent un lien entre un manque de sommeil et le développement de maladie ainsi que la réduction de l'espérance de vie.</p>
+<p>Une étude faite sur des rats (<a rel="nofollow" target="_blank" href="https://www.ncbi.nlm.nih.gov/pubmed/9989363">ici</a>) montre qu'un groupe de rat privé du sommeil paradoxale vie en moyenne 3 semaines. Tandis que l'espérance de vie d'un rat est de 2/3ans.</p>
+<p>C'est quand même choquant. Bien sûre, on n'est pas des rats, mais est-on invulnérable au manque de sommeil ?</p>
+<p>Je ne parierai pas là-dessus.</p>
+
+<p>On a vu l'importance d'un sommeil de qualité ainsi qu'une bonne alimentation pour récupérer au mieux.</p>
+<p>Je vais vous guider pour avoir le meilleur sommeil possible.</p>
+
+<h2>Voici comment avoir un sommeil réparateur</h2>
+
+<p>Un point très important dans ce cas est la régularité de vos heures de sommeil.</p>
+
+<p>Le corps possède sa propre horloge interne qui lance différent processus interne tout au long de ces 24h.</p>
+Comme l'envie d'aller dormir, de manger, de se reposer ...</p>
+
+<p>L'erreur à ne pas faire pour bien récupérer est de ne pas prendre en compte notre horloge interne.</p>
+<p>Si pour votre horloge interne vous devez aller dormir à 22h30 et que vous ne la respectée pas, en vous couchant à 00h00.</p>
+<p>Le processus de nettoyage et régénération va être perturbé.</p>
+<p>Vous allez manquer le premier cycle de 90min où la phase de sommeil profond est la plus importante et où on secrète davantage d'hormone de croissance.</p>
+<p>Vous allez directement commencer au deuxième cycle de sommeil...</p>
+
+<p>De plus à l'heure de réveil de l'horloge interne. Les heures de sommeil supplémentaire que vous ferez ne sont pas très efficaces. C'est un sommeil de mauvaise qualité.</p>
+
+<p>C'est pourquoi je vous conseille, de favoriser au maximum une heure de coucher régulière. Sinon vous accepter de ne pas avoir un sommeil récupérateur comme il se le doit.</p>
+
+<p>Choisir, c'est renoncer. Je vous informe et vous en faîte ce que vous voulez.</p>
+
+<h3>Récupérer le bonus</h3>
+<p>Si vous dormez mal, que vous vous réveillé fatigué, que avez du mal à vous endormir, que vous manquez d'énergie dans la journée ou que vous voulez améliorer votre qualité de sommeil.</p>
+<p>Vous pouvez commencer par récupérer mon bonus qui se trouve à la fin de cet article. Il donne les clés pour un sommeil profond.</p>
+<p>C'est un excellent guide pour avoir une bonne hygiène de sommeil.</p>
+<p>De plus, si vous voulez aller plus loin. N'hésitez pas à me suivre et à lire régulièrement mes articles.</p>
+
+<p>Je vous parlais de l'horloge interne du corps, et bien il y a pleins de choses à savoir dessus en la respectant pour optimiser notre énergie/créativité/performance.</p>
+<p>J'étais stupéfait des changements que mon apporter cette harmonie avec l'horloge interne.</p>
+
+<p>Je vous partagerai tout cela plus tard.</p>
