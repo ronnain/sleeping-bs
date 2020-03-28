4d46c54a9bc1726b2feda4be4c7851fa9d90a464
unknown <romain.geffrault@gmail.com>
2020-03-28 14:23:18 +0100

    orthographe corrections

--- articles/comment-avoir-un-sommeil-réparateur.php
+++ articles/comment-avoir-un-sommeil-réparateur.php
@@ -9,14 +9,14 @@ header('Access-Control-Allow-Headers: responsetype');
 <img class="imgBook noMarginBottom" src="assets/img/sommeil-reparateur.jpg" alt="Tête réparé" >
 <div class="creditImgDiv"><a class="creditImg" href="https://fr.freepik.com/photos-vecteurs-libre/fond" target="_blank" rel="nofollow">Lien Créateur Image</a></div>
 
-<p>Avoir un sommeil réparateur est très important que se soit pour se sentir bien, se sentir en forme, être prêts à affronter la journée, avoir récupéré des efforts de la veille, pour apprendre et faire le trie des informations de la journée.</p>
+<p>Avoir un sommeil réparateur est très important, que ce soit pour se sentir bien, se sentir en forme, être prêt à affronter la journée, récupérer des efforts de la veille, pour apprendre et faire le tri des informations de la journée.</p>
 <p>À l'inverse mal dormir, ne permet pas de bénéficier de tous les effets d'un bon sommeil.</p>
-<p>Ca tombe bien, nous allons voir dans cet article comment avoir un sommeil réparateur !</p>
+<p>Cela tombe bien, nous allons voir dans cet article comment avoir un sommeil réparateur !</p>
 
 <h2>Sommeil réparateur où es-tu?</h2>
 <p>Ayant eu pendant longtemps des problèmes de sommeil.</p>
-<p>Je me suis intéressé à comment je pouvait faire pour avoir un sommeil réparateur.</p>
-<p>Je voulais simplement me réveiller le matin avec toute mon énergie et avoir bien récupéré des efforts de la veille.</p>
+<p>Je me suis intéressé à comment je pouvais faire pour avoir un sommeil réparateur.</p>
+<p>Je voulais simplement me réveiller le matin avec toute mon énergie et en ayan récupéré des efforts de la veille.</p>
 <p>Je me suis d'abord questionné sur l'effet réparateur du sommeil.</p>
 
 <h2>Comment le corps récupère ?</h2>
@@ -28,9 +28,9 @@ header('Access-Control-Allow-Headers: responsetype');
 
 
 <h3>Déchets générés par le cerveau</h3>
-<p>Une étude récente (<a rel="nofollow" target="_blank" href="https://science.sciencemag.org/content/342/6156/373">ici</a>) à montrer que le cerveau possède son propre système de nettoyage.</p>
+<p>Une étude récente (<a rel="nofollow" target="_blank" href="https://science.sciencemag.org/content/342/6156/373">ici</a>) a démontré que le cerveau possède son propre système de nettoyage.</p>
 <p>Il s'agit du système glymphatique.</p>
-<p>Ca fait très médicale comme nom et ce n'est pas très parlant.</p>
+<p>Cela fait très médical comme appellation et ce n'est pas très parlant.</p>
 <p>On va prendre l'exemple des éboueurs.</p>
 <p>Le système glymphatique va représenter un camion poubelle.</p>
 <p>Il va être alimenté par les éboueurs qui circulent dans l'espace entre les différentes cellules du cerveau à la recherche des déchets.</p>
@@ -49,27 +49,27 @@ header('Access-Control-Allow-Headers: responsetype');
 <p>Une fois remplis les camions poubelles vont prendre l'autoroute en passant par le sang.</p>
 <p>Leur destination est le foie qui fait office de déchetterie.</p>
 
-<h3>Le foie alias Anti-poison Man</h3>
+<h3>Le foie alias Anti-Poison Man</h3>
 
 <p>Le foie se charge de traiter l'ensemble des toxines du corps.
-<p>Il va les rendre moins nocives et les rejeter à grâce aux fameux pipi/caca.</p>
-<p>Le foie est un super héros (Anti-poison Man). En plus d'éliminer les toxines des cellules.</p>
+<p>Il va les rendre moins nocives et les rejeter grâce... enfin aux toilettes quoi.</p>
+<p>Le foie est un super héros (Anti-Poison Man). En plus d'éliminer les toxines des cellules.</p>
 <p>Il va aussi traiter les produits nocifs qui sont passés dans notre corps, comme par l'alimentation.</p>
 <p>Exhausteur de goûts, colorants, antibiotiques (se trouvant dans la viande) et les métaux lourds.</p>
-<p>Les produits chimiques d'entretien, cosmétique, alcool, drogue, médicaments, la poulltion de l'air passée par nos poumons...</p>
+<p>Les produits chimiques d'entretien, cosmétiques, alcool, drogue, médicaments, la pollution de l'air passée par nos poumons...</p>
 
 <p>Il faut donc veiller au bon fonctionnement de notre foie en apportant une alimentation saine et équilibré pour qu'il puisse se concentrer davantage sur l'élimination des déchets du corps.</p>
 
 <h3>Régénérer nos tissus</h3>
-<p>Tout au long de la journée nos tissus cellulaire comme les muscles se sont abîmés.</p>
-<p>Le sommeil joue un rôle esentiel pour la réparation de nos tissus.</p>
+<p>Tout au long de la journée nos tissus cellulaires comme les muscles se sont abîmés.</p>
+<p>Le sommeil joue un rôle essentiel pour la réparation de nos tissus.</p>
 <p>Comme le nettoyage des déchets, la réparation est d'autant plus efficace la nuit. D'une part car nous ne faisons plus d'activité physique qui abîme les tissus.</p>
-<p>Mais aussi parce que le corps va secreter un cocktail d'hormone spécialement pour se réparer et se consolider.</p>
+<p>Mais aussi parce que le corps va sécréter un cocktail d'hormone spécialement pour se réparer et se consolider.</p>
 <p>Comme l'hormone de croissance, une des plus importante et puissante du corps.</p>
 
-<p>Voyons un peu ce que fait l'hormone de croissance.</p>
+<p>Voyons un peu quel est le rôle de l'hormone de croissance.</p>
 
-<p>Lorsque l'on est bébé, on secrète énormément d'hormone de croissance afin de nous aider à grandir.</p>
+<p>Lorsque l'on est bébé, on sécr"te énormément d'hormone de croissance afin de nous aider à grandir.</p>
 <p>Puis à l'adolescence le corps produit un 2ième pic, encore une fois pour grandir.</p>
 <p>Ensuite, le corps continu à produire de l'hormone de croissance, mais à plus faible dose, pour se maintenir.</p>
 
@@ -89,12 +89,12 @@ header('Access-Control-Allow-Headers: responsetype');
 <p>Par contre, les effets négatifs peuvent être très graves, voir mortels.</p>
 <p>Tant que vous laisser le corps faire, vous ne risquez rien.</p>
 <p>Le corps sait s'auto réguler pour fonctionner au mieux.</p>
-<p>C'est lorsqu'une hormone vient d'autre part que par le corps que ça devient très dangereux. C'est un peu faire un pacte avec le diable.</p>
+<p>C'est lorsqu'une hormone vient d'ailleurs que par le corps que cela devient très dangereux. C'est un peu faire un pacte avec le diable.</p>
 <p>C'est ce qu'on appelle le dopage.</p>
 <p>Je suis totalement contre ce genre de pratique qui n'est pas bénéfique pour le bien-être et la bonne santé sur le long terme.</p>
 <p>De plus, les raisons d'un tel acte sont très souvent mesquines et hypocrites.</p>
 <p>Actuellement, on peut voir beaucoup d'influenceurs (homme et femme) du fitness qui se dopent, mais qui le cachent.</p>
-<p>Ils mettent en avant leurs résultats grâce à leur méthode d'entraînements et leurs assiduités...</p>
+<p>Ils mettent en avant leurs résultats grâce à leur méthode d'entraînement et leur assiduitée...</p>
 <p>Bref attention à ce genre de personne. Vous n'aurez jamais leurs résultats et vous risquez de vous abîmer.</p>
 
 	<p class="citation">10% de sportifs amateurs sont dopés</p>
@@ -122,34 +122,34 @@ header('Access-Control-Allow-Headers: responsetype');
 
 <h3>Phases 3-4: Sommeil Profond</h3>
 <p>Dans cette phase, le corps ne bouge plus, sa température est la plus basse.</p>
-<p>C'est à cette phase que le corps produits le plus d'hormones dont l'hormone de croissance.</p>
+<p>C'est lors de cette phase que le corps produit le plus d'hormones dont l'hormone de croissance.</p>
 <p>Ces phases représentent 20% du sommeil.</p>
 <p>À noter que c'est lors du premier cycle où elle dure le plus longtemps.</p>
 <p>Puis, plus les cycles avancent plus les phases de sommeil profond sont courtes et laissent place à plus de sommeil paradoxal.</p>
 
-<p>C'est donc cette phase-là qu'il faut absolument faire pour que le corps puisse récupérer au mieux.</p>
+<p>C'est donc cette phase-là qu'il ne faut absolument pas louper pour que le corps puisse récupérer au mieux.</p>
 <p>Car oui, on peut la louper.</p>
 <p>On va voir ça plus loin.</p>
 
 <h3>Phase 5: Sommeil paradoxal</h3>
-<p>C'est la phase où l'on rêve. On passe en moyenne 2h par nuit à rêver, mais si on ne s'en souvient rarement.</p>
-<p>A l'inverse du sommeil profond, plus les cycles du sommeil avance plus le temps passé en sommeil paradoxal augmente.</p>
+<p>C'est la phase où l'on rêve. On passe en moyenne 2h par nuit à rêver, même si on ne s'en souvient que rarement.</p>
+<p>A l'inverse du sommeil profond, plus les cycles du sommeil avancent plus le temps passé en sommeil paradoxal augmente.</p>
 <p>Jusqu'à 45min de sommeil paradoxal, soit la moitié d'un cycle.</p>
 <p>C'est aussi une phase très importante pour récupérer, car c'est grâce à cette phase que l'on apprend.</p>
 <p>Le sommeil paradoxal sollicite énormément les parties du cerveau qui génèrent nos pensées et qui organisent les données.</p>
 <p>Les bébés passent plus de temps en phase de sommeil paradoxal, c'est ce qui leur permet d'apprendre énormément.</p>
 
-<p>Cette étude (<a rel="nofollow" target="_blank" href="https://www.ncbi.nlm.nih.gov/pubmed/25447376">ici</a>), montre que les personnes qui sont privé de cette phase de sommeil n'apprenne pas ce qu'ils ont appris à faire la veille.</p>
-<p>Hors ceux privé du sommeil profond ont appris ce qu'ils ont vue la veille.</p>
+<p>Cette étude (<a rel="nofollow" target="_blank" href="https://www.ncbi.nlm.nih.gov/pubmed/25447376">ici</a>), montre que les personnes qui sont privées de cette phase de sommeil n'apprennent pas ce qu'ils ont appris à faire la veille.</p>
+<p>Hors ceux privés du sommeil profond ont appris ce qu'ils ont vu la veille.</p>
 
-<p>En période d'examen, penser à ça et si les bénéfices d'une nuit blanche avec l'examen est une bonne idée.</p>
+<p>En période d'examen, penser aux conséquences d'une nuit blanche.</p>
 
-<p>On peut se dire ok, ça permet d'apprendre, mais à mon âge, je n'ai plus besoin d'apprendre. Je peux me passer du sommeil paradoxal.</p>
+<p>On peut se dire ok, cela permet d'apprendre, mais à mon âge, je n'ai plus besoin d'apprendre. Je peux me passer du sommeil paradoxal.</p>
 <p>Eh bien, je vous dirai, c'est une excellente idée pour se saboter la santé.</p>
 
-<p>Beaucoup d'étude montrent un lien entre un manque de sommeil et le développement de maladie ainsi que la réduction de l'espérance de vie.</p>
-<p>Une étude faite sur des rats (<a rel="nofollow" target="_blank" href="https://www.ncbi.nlm.nih.gov/pubmed/9989363">ici</a>) montre qu'un groupe de rat privé du sommeil paradoxale vie en moyenne 3 semaines. Tandis que l'espérance de vie d'un rat est de 2/3ans.</p>
-<p>C'est quand même choquant. Bien sûre, on n'est pas des rats, mais est-on invulnérable au manque de sommeil ?</p>
+<p>Beaucoup d'études démontrent un lien entre un manque de sommeil et le développement de maladies ainsi que la réduction de l'espérance de vie.</p>
+<p>Une étude faite sur des rats (<a rel="nofollow" target="_blank" href="https://www.ncbi.nlm.nih.gov/pubmed/9989363">ici</a>) montre qu'un groupe de rats privés du sommeil paradoxale vie en moyenne 3 semaines. Tandis que l'espérance de vie d'un rat est de 2/3ans.</p>
+<p>C'est quand même choquant. Bien sûr, nous ne sommes pas des rats, mais sommes-nous invulnérables au manque de sommeil pour autent?</p>
 <p>Je ne parierai pas là-dessus.</p>
 
 <p>On a vu l'importance d'un sommeil de qualité ainsi qu'une bonne alimentation pour récupérer au mieux.</p>
@@ -157,31 +157,30 @@ header('Access-Control-Allow-Headers: responsetype');
 
 <h2>Voici comment avoir un sommeil réparateur</h2>
 
-<p>Un point très important dans ce cas est la régularité de vos heures de sommeil.</p>
+<p>Le point le plus important, à ne surtout pas négliger: la régularité de vos heures de sommeil.</p>
 
-<p>Le corps possède sa propre horloge interne qui lance différent processus interne tout au long de ces 24h.</p>
-Comme l'envie d'aller dormir, de manger, de se reposer ...</p>
+<p>Le corps possède sa propre horloge interne, au quotidien elle orchestre différents processus interne. Tel que le sommeil, la digestion...</p>
 
 <p>L'erreur à ne pas faire pour bien récupérer est de ne pas prendre en compte notre horloge interne.</p>
-<p>Si pour votre horloge interne vous devez aller dormir à 22h30 et que vous ne la respectée pas, en vous couchant à 00h00.</p>
+<p>Si pour votre horloge interne vous devez aller dormir à 22h30 et que vous ne la respectez pas, en vous couchant à 00h00.</p>
 <p>Le processus de nettoyage et régénération va être perturbé.</p>
-<p>Vous allez manquer le premier cycle de 90min où la phase de sommeil profond est la plus importante et où on secrète davantage d'hormone de croissance.</p>
+<p>Vous allez manquer le premier cycle de 90min où la phase de sommeil profond est la plus importante et l'où on sécréte davantage d'hormones de croissance.</p>
 <p>Vous allez directement commencer au deuxième cycle de sommeil...</p>
 
-<p>De plus à l'heure de réveil de l'horloge interne. Les heures de sommeil supplémentaire que vous ferez ne sont pas très efficaces. C'est un sommeil de mauvaise qualité.</p>
+<p>De plus à l'heure de réveil de l'horloge interne. Les heures de sommeil supplémentaires que vous ferez ne sont pas très efficaces. C'est un sommeil de mauvaise qualité.</p>
 
-<p>C'est pourquoi je vous conseille, de favoriser au maximum une heure de coucher régulière. Sinon vous accepter de ne pas avoir un sommeil récupérateur comme il se le doit.</p>
+<p>C'est pourquoi je vous conseille, de favoriser au maximum une heure de coucher régulière. Sinon vous accepter de ne pas avoir un sommeil récupérateur comme il se doit.</p>
 
-<p>Choisir, c'est renoncer. Je vous informe et vous en faîte ce que vous voulez.</p>
+<p>Choisir, c'est renoncer. Je vous informe et vous en faîtes ce que vous voulez.</p>
 
 <h3>Récupérer le bonus</h3>
-<p>Si vous dormez mal, que vous vous réveillé fatigué, que avez du mal à vous endormir, que vous manquez d'énergie dans la journée ou que vous voulez améliorer votre qualité de sommeil.</p>
-<p>Vous pouvez commencer par récupérer mon bonus qui se trouve à la fin de cet article. Il donne les clés pour un sommeil profond.</p>
+<p>Si vous dormez mal, que vous vous réveillez fatigué, que avez du mal à vous endormir, que vous manquez d'énergie dans la journée ou que vous voulez améliorer votre qualité de sommeil.</p>
+<p>Vous pouvez commencer par télécharger mon bonus qui se trouve à la fin de cet article. Il donne les clés pour un sommeil profond.</p>
 <p>C'est un excellent guide pour avoir une bonne hygiène de sommeil.</p>
 <p>De plus, si vous voulez aller plus loin. N'hésitez pas à me suivre et à lire régulièrement mes articles.</p>
 
-<p>Je vous parlais de l'horloge interne du corps, et bien il y a pleins de choses à savoir dessus en la respectant pour optimiser notre énergie/créativité/performance.</p>
-<p>J'étais stupéfait des changements que mon apporter cette harmonie avec l'horloge interne.</p>
+<p>Je vous parle de l'horloge interne du corps, il y a pleins de choses à savoir dessus en la respectant pour optimiser notre énergie/créativité/performance.</p>
+<p>J'étais stupéfait des changements que m'a apporté cette harmonie avec l'horloge interne.</p>
 
 <p>Je vous partagerai tout cela plus tard.</p>
 
---
+++ articles/passage-heure-été-7-astuces.php
@@ -0,0 +1,168 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, OPTIONS');
+header('Access-Control-Allow-Headers: responsetype');
+?>
+
+<h1>Passage à l'heure d'été - 7 Astuces faciles pour ne pas subir 1h de sommeil en moins</h1>
+
+<img class="imgBook noMarginBottom" src="assets/img/time-changed.jpg" alt="Grande horlogoe" >
+<div class="creditImgDiv"><a class="creditImg" href="https://fr.freepik.com/photos-vecteurs-libre/affaires" target="_blank" rel="nofollow">Lien Créateur Image</a></div>
+
+	<p>Perdre 1 heure de sommeil, ce n'est clairement pas agréable.</p>
+	<p>Contrairement, au changement d'heure hivernal, ce décalage fait déprimer les grands dormeurs et les couches-tard.</p>
+	<p>Des études montrent, un taux de déprime, d'accidents et de suicides plus important la semaine qui suit le changement d'heure.</p>
+	<p>Heureusement, j'ai 7 astuces faciles à mettre en place pour vous aider à mieux vous adapter sur cette période.</p>
+	<p>Si vous les mettez correctement en place, vous ne devrez même pas sentir le changement d'heure.</p>
+	<p>Ces conseils sont utiles tant que vous vous sentez fatigué de cette transition.</p>
+
+<h2>Perdre 1h de sommeil</h2>
+
+	<p>Prenons un exemple.</p>
+	<p>Habituellement, vous vous couchez à 00h00 et vous mettez un réveil à 7h00.</p>
+	<p>Cela vous fait 7h de sommeil.</p>
+	<p>Reprenons l'exemple, la nuit du changement d'heure.</p>
+	<p>Vous faîte la même chose, vous vous couchez à 00h00 et vous vous levez à 7h00.</p>
+	<p>Vous aurez dormi seulement 6h.</p>
+	<p>À 2h00 du matin, on avancera directement à 3h00.</p>
+	<p>Donc, c'est une heure de sommeil perdue.</p>
+
+
+	<p>Un des principaux objectifs de ces astuces va être de naturellement vouloir aller au lit plus tôt quand votre corps vous le dira.</p>
+	<p>Si vous n'y arrivez pas ce n'est pas grave.</p>
+	<p>En suivant ces conseils, vous devriez avoir un sommeil de meilleure qualité et en ressentir les effets positifs.</p>
+
+<h3>Le corps déboussolé</h3>
+	<p>Le corps possède une horloge interne calée sur 24h. Elle permet de lancer les différents processus au fur et à mesure de la journée.</p>
+	<p>Elle dicte au cerveau quand vous réveiller, quand vous devez manger, vous coucher...</p>
+	<p>C'est l'horloge circadienne.</p>
+	<p>Elle se base principalement sur le rythme du soleil.</p>
+	<p>Elle est une pièce centrale dans le bon fonctionnement de notre organisme.</p>
+	<p>Elle aime surtout la régularité.</p>
+	<p>Sinon elle est perdue, la pauvre.</p>
+	<p>Si tout ne va pas comme elle veut, vous allez en ressentir indirectement les conséquences.</p>
+	<p>Fatigue, mauvaise humeur...</p>
+
+<h3>Durée d'adaptation du corps</h3>
+	<p>Pour une majorité de personnes, l'adaptation va se faire progressivement.</p>
+	<p>Au bout d'une semaine, le corps sera adapté à cette nouvelle heure.</p>
+	<p>Mais ce temps d'adaptation peut augmenter vos chances d'avoir un accident dû à la fatigue.</p>
+	<p>Il est donc intéressant de faire plus attention et d'aider son corps à faire la transition.</p>
+
+
+<h2>7 Astuces pour ne pas subir 1h de sommeil en moins</h2>
+
+<h3>Astuce 1 - Profiter de la lumière naturelle</h3>
+	<p>Pour cela, on va s'appuyer sur le rythme du soleil.</p>
+	<p>Comme on l'a vu, l'horloge interne se cale sur le rythme solaire.</p>
+	<p>Je te recommande donc de t'exposer le plus possible à la lumière naturelle surtout l'après-midi et avant que le soleil ne se couche.</p>
+	<p>Fais au moins 2 sessions de 15 minutes chacune dont une proche de 18h30, quand le soleil commence à se coucher.</p>
+	<p>S'il ne fait pas beau, pas de soucis reste proche d'une fenêtre à regarder dehors.</p>
+	<p>C'est important que vos yeux reçoivent cette lumière.</p>
+	<p>La lumière naturelle même par temps pluvieux est beaucoup plus puissante que la lumière d'intérieur, le corps sait la reconnaître.</p>
+	<p>Évite de rester devant un écran pendant ce temps.</p>
+	<p>Tu peux discuter, téléphoner...</p>
+	<p>Répète cette astuce toute la semaine qui suit le changement d'heure.</p>
+
+<h3>Astuce 2 - Favoriser des lumières tamisées dans son logement</h3>
+	<p>Une fois le soleil couché, essaie de limiter les sources de lumière.</p>
+	<p>Essaie de garder une ambiance tamisée pour que ton corps comprenne que le soleil s'est couché et qu'il faut ralentir.</p>
+	<p>Cela permet de faire une démarcation avec les activités de la journée.</p>
+	<p>Personnellement, je trouve cela très agréable.</p>
+	<p>Je n’apprécie pas lorsqu'il y a trop de lumière le soir.</p>
+	<p>J'ai l'impression d'avoir trop de sollicitations.</p>
+
+<h3>Astuce 3 - Télé & jeux vidéo - Ok, mais pas trop tard</h3>
+<p>Je pense que c'est le sujet le plus sensible pour vous aider à passer cette transition.</p>
+	<p>C'est très facile le soir, de regarder la télé et ne plus faire attention à l'heure.</p>
+	<p>On peut facilement enchaîner les épisodes (merci, Netflix).</p>
+	<p>Résultat, on se couche plus tard que prévu.</p>
+	<p>Malheureusement, dans notre cas, il va falloir absolument éviter ça.</p>
+	<p>Sinon tous tes efforts de la journée tombent à l'eau.</p>
+	<br/>
+	<p>L'objectif est le suivant: 1h30 avant ton heure de coucher habituelle, dit stop à tous les écrans.</p>
+	<p>Le problème des écrans est leur émission de la fameuse lumière bleue qui naturellement provient du soleil.</p>
+	<p>Ils vont donc tromper ton horloge interne sur le rythme du soleil, et cela te fera aller au lit plus tard.</p>
+
+	<p>À la place, tu peux lire, jouer aux jeux de sociétés/cartes, discuter, téléphoner...</p>
+	<p>C'est moins cool, mais tu peux en profiter pour ranger, nettoyer...</p>
+
+	<p>C’est également le moment idéal pour s’adonner au plaisir charnel.</p>
+	<p>Même pratiqué seul, l'orgasme à des effets positifs incroyables sur la santé.</p>
+	<p>Et dans ce cas, cela t'aidera à te relaxer et à dormir plus rapidement.</p>
+
+<h3>Astuce 4 - Soirée spéciale relaxation</h3>
+
+	<p>Pour appeler le sommeil, il faut se détendre.</p>
+	<p>Comme dit juste avant, l'orgasme est très bon pour ça.</p>
+	<p>Dans l'heure qui précède votre aller au lit.</p>
+	<p>Vous pouvez ralentir en faisant un peu de méditation, yoga...</p>
+	<p>Pour méditer, c'est très simple.</p>
+	<p>Voici quelques conseils que j’utilise au quotidien :</p>
+	<ul>
+		<li><p>Je préviens mon entourage et je coupe mon portable, pour ne pas être dérangé pendant la séance.</p></li>
+		<li><p>Je lance un chronomètre de 5 min.</p></li>
+		<li><p>Je m'assis sur une chaise dans un endroit calme.</p></li>
+		<li><p>Je ferme les yeux.</p></li>
+		<li><p>Je me concentre sur ma respiration.</p></li>
+		<li><p>J'essaie d'avoir une respiration calme et profonde.</p></li>
+		<li><p>Je compte mes respirations de 0 à 10 puis de 10 à 0.</p></li>
+		<li><p>Dès qu'une pensée arrive, j'essaie de me rendre compte que je ne suis plus concentré sur ma respiration. Puis je reprend à 0 mon comptage.</p></li>
+		<li><p>Si, j'ai réussi l'exercice. Je profite du temps restant pour ressentir mon corps détendue.</p></li>
+	</ul>
+	<p>Si tu n’oses pas te lancer seul, tu peux aussi t’initier à la méditation guidée, en utlisant une application gratuite comme Petit Bambou.</p>
+	<p>Disponible sur toutes les plateformes mobiles.</p>
+
+<h3>Astuce 5 - Prendre un bain ou une douche chaude</h3>
+	<p>Pendant l'heure de relaxation, tu peux prendre un bain chaud ou une douche chaude.</p>
+	<p>Cela va permettre d'aider ton corps à se refroidir.</p>
+	<p>Cela peut paraître contre intuitif.</p>
+
+	<p>Mais le corps agit comme un régulateur de température.</p>
+	<p>Si tu prends une douche froide. Ton corps va se mettre à chauffer davantage pour garder sa température.</p>
+	<p>Tandis que si tu prends une douche chaude. Il va se mettre à se refroidir pour contrer le chaud.</p>
+
+	<p>Avant de dormir, le corps expulse la chaleur pour ralentir.</p>
+	<p>Souvent, avant de s'endormir, on peut ressentir ses pieds, ses mains et ses oreilles toutes chaudes.</p>
+	<p>C'est que vous entamez le processus de sommeil.</p>
+
+<h3>Astuce 6 - Café & Thé, seulement le matin</h3>
+	<p>Peut-être un autre point sensible surtout chez des consommateurs de café.</p>
+	<p>Le café et le thé ont des répercussions plusieurs heures après l'ingestion.</p>
+	<p>On consomme le café et le thé pour sentir un coup de boost contre la fatigue.</p>
+	<p>Bien qu'on ne ressentte pas les effets très longtemps.</p>
+	<p>Le corps met beaucoup de temps à les éliminer et cela impacte d'autres fonctionnements dont le sommeil.</p>
+	<p>Le pire, c'est que l'on ne s'en rend pas compte. Surtout si on est habitué à en consommer.</p>
+	<br/>
+	<p>Pour cette période, je te recommande d'éviter le café et le thé.</p>
+	<p>Si tu ne peux pas faire sans, tu peux en prendre 1 tasse le matin.</p>
+
+	<p>Tu te sens fatigué l'après-midi? Cela ne sera que passager, au pire essaie de faire une sieste chronométré de 20min pas plus.</p>
+	<p>Sinon tu vas te réveiller déboussolé.</p>
+
+	<p class="citation">PS: Tu peux aussi consommer du café décaféiné et des infusions.</p>
+
+<h3>Astuce 7 - Le dîner</h3>
+	<p>Toujours pour aider votre corps à bien lancer le processus de sommeil.</p>
+	<p>Essaie d'espacer de minimum 2h ton dernier repas de l'heure du coucher.</p>
+	<p>L'estomac a besoin de beaucoup d'énergie pour digérer.</p>
+	<p>Il vaut mieux lui laisser le temps de faire son principal travail.</p>
+	<p>Une fois, que l'estomac a fait son job.</p>
+	<p>Le corps peut enfin ralentir.</p>
+</p>
+
+<h3>Laisser venir le sommeil</h3>
+	<p>Quand tu sens le sommeil te gagner, n'essaie pas de lutter et d'attendre ton heure habituelle de coucher.</p>
+	<p>Va te coucher.</p>
+	<p>Les actions positives que tu as fait durant la soirée, vont te permettre d'avoir un sommeil de meilleure qualité.</p>
+
+<h2>Peut-être as-tu des problèmes de sommeil ?</h2>
+	<p>Suivre et mettre en place ces premières astuces devraient fortement t'aider à mieux dormir.</p>
+	<p>Si tu veux aller encore plus loin, car tu veux avoir un sommeil réparateur, te sentir moins fatigué la journée...</p>
+	<p>N'hésite pas à télécharger mon bonus (ci-dessous) où je partage encore plus de conseils.</p>
+	<p>Tu peux lire les autres articles de ce blog et me suivre.</p>
+	<p>J'accompagne les personnes à résoudre leur problème de sommeil.</p>
+	<p>J'espère que tu y trouveras ta réponse.</p>
+
+
+<footer class="blockquote-footer">Ecrit le 28/03/2020 par <cite>Romain</cite></footer>
